64308b257789388542be104937ec49a5acaa7939
CesarBaquiro <cmbm.kf@gmail.com>
2023-08-07 17:18:09 -0500

    Add Whatsapp message and link generation functionality

--- src/app/form-order/form-order.component.ts
+++ src/app/form-order/form-order.component.ts
@@ -29,7 +29,58 @@ export class FormOrderComponent implements OnInit {
       customerInfo[key] = value;
     });
 
-    console.log('Información del formulario:', customerInfo);
-    console.log('Informacion del carrito', this.cartProducts);
+    const customerInfoStr = `${customerInfo['nombreApellido']}
+${customerInfo['email']}
+${customerInfo['telefono']}
+`;
+    console.log(customerInfoStr);
+    //customerInfoStr += infoCliente + '\n'; // Agregar la información del pedido a pedidoInfo
+
+    // -----------Imprimir lista de pedidos
+    let pedidoStr = '';
+
+    this.cartProducts.forEach((product) => {
+      let pedido: string = `${product.titulo} - Cantidad: ${
+        product.cantidad
+      } - $${product.precio * product.cantidad}`;
+      pedidoStr += pedido + '\n'; // Agregar la información del pedido a pedidoInfo
+    });
+
+    const whatsappLink: string = createWhatsAppLink(
+      `*Nuevo pedido - Chips*
+---
+*Productos*
+${pedidoStr}
+---
+*Datos del comprador*
+${customerInfoStr}
+---
+*Informacion de envio*
+
+Dirección de entrega:
+${customerInfo['direccion']},
+${customerInfo['ciudad']}
+
+Detalles adicionales:
+El envío es por parte de una empresa transportadora nacional, cualquier inconveniente es responsabilidad de la empresa y se hará acompañamiento en su debido proceso.
+---
+
+
+-Acepta terminos y condiciones.-`
+    );
+    console.log(whatsappLink);
   }
+}
+
+//-------------------LINK DE WHATSAPP-------------
+
+function createWhatsAppLink(message: string): string {
+  // Reemplaza los caracteres especiales en el mensaje
+  const encodedMessage: string = encodeURIComponent(message);
+
+  // Crea el enlace de WhatsApp con el mensaje
+  const link: string =
+    'https://api.whatsapp.com/send?phone=573005228767&text=' + encodedMessage;
+
+  return link;
 }
